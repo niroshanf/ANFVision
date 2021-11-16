5872c8764bcffa4ecdccadba9d0d07893dd558bf
Niroshan Fernandez <niroshanf@gmail.com>
2021-11-16 11:37:36 +0400

    Initial commit

---
+++ ANFVision/ANFVision.h
@@ -0,0 +1,18 @@
+//
+//  ANFVision.h
+//  ANFVision
+//
+//  Created by Anthony Niroshan Fernandez on 16/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ANFVision.
+FOUNDATION_EXPORT double ANFVisionVersionNumber;
+
+//! Project version string for ANFVision.
+FOUNDATION_EXPORT const unsigned char ANFVisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANFVision/PublicHeader.h>
+
+
