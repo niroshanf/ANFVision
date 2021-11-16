1d83b9ad9e5cb94dd75639ba6c2563110971095a
Niroshan Fernandez <niroshanf@gmail.com>
2021-11-16 12:23:19 +0400

    Added podspec file

---
+++ ANFVision/Classes/ANFVisionManager.swift
@@ -0,0 +1,13 @@
+//
+//  ANFVisionManager.swift
+//  ANFVision
+//
+//  Created by Anthony Niroshan Fernandez on 16/11/2021.
+//
+
+import Foundation
+
+public class ANFVisionManager {
+    
+    
+}
